8223cbcc484267b7434dcf12d62e373e5ece2e58
parthu1029 <parthasaradhiganta@gmail.com>
2026-03-04 00:34:01 +0530

    fix port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/verify-otp", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
